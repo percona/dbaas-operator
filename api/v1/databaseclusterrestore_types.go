e176ca103cbccaa876f826e992a7a1b15ddc6504
Andrew Minkin <andrew.minkin@percona.com>
2022-12-26 18:32:33 +0600

    PMM-10980 Added Backup/Restore kinds

---
+++ api/v1/databaseclusterrestore_types.go
@@ -0,0 +1,63 @@
+// dbaas-operator
+// Copyright (C) 2022 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DatabaseClusterRestoreSpec defines the desired state of DatabaseClusterRestore
+type DatabaseClusterRestoreSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of DatabaseClusterRestore. Edit databaseclusterrestore_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// DatabaseClusterRestoreStatus defines the observed state of DatabaseClusterRestore
+type DatabaseClusterRestoreStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// DatabaseClusterRestore is the Schema for the databaseclusterrestores API
+type DatabaseClusterRestore struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DatabaseClusterRestoreSpec   `json:"spec,omitempty"`
+	Status DatabaseClusterRestoreStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// DatabaseClusterRestoreList contains a list of DatabaseClusterRestore
+type DatabaseClusterRestoreList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DatabaseClusterRestore `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DatabaseClusterRestore{}, &DatabaseClusterRestoreList{})
+}
