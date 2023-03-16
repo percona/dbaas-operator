fd593a5b89551717195bc55dcf4b978ecdb01211
Andrew Minkin <andrew.minkin@percona.com>
2023-03-16 15:05:35 +0600

    Fixed main.go

--- main.go
+++ main.go
@@ -14,8 +14,7 @@
 // limitations under the License.
 
 // Package main ...
-//
-//nolint:gci
+
 package main
 
 import (
@@ -27,7 +26,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -37,7 +35,6 @@ import (
 
 	dbaasv1 "github.com/percona/dbaas-operator/api/v1"
 	"github.com/percona/dbaas-operator/controllers"
-	//+kubebuilder:scaffold:imports
 )
 
 const watchNamespaceEnvVar = "WATCH_NAMESPACE"
